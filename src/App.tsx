65476b032a44aeff84a86ec5394446e73f266b90
Nitael <nitael@okaydev.com.br>
2022-01-07 11:32:52 -0300

    animation finished(case 1)

--- src/App.tsx
+++ src/App.tsx
@@ -9,46 +9,54 @@ function App() {
     <div className="App">
         <div
           onClick={() => {
+            if (hamburger === 2) {
+              return;
+            }
             setHamburger(0);
-            setTimeout(() => {setHamburger(1)}, 450);
-            setTimeout(() => {setHamburger(2)}, 455);
+            setTimeout(() => {setHamburger(1)}, 290);
+            setTimeout(() => {setHamburger(2)}, 300);
           }}
           className={`scale-container${
-            hamburger === 0? " move" : hamburger > 0 ? " reset" : ""
+            hamburger === 0? " move" : hamburger === 4? ' reverse' : hamburger > 0 ? " reset" :  ''
           }`}
         >
           <div
             className={`hamburger-white-bar top${
-              hamburger === 0? " move" : hamburger > 0 ? " reset" : ""
+              hamburger === 0? " move" : hamburger === 4? ' reverse' : hamburger > 0 ? " reset" :  ''
             }`}
           />
           <div
             className={`hamburger-white-bar center${
-              hamburger === 0? " move" : hamburger > 0 ? " reset" : ""
+              hamburger === 0? " move" : hamburger === 4? ' reverse' : hamburger > 0 ? " reset" :  ''
             }`}
           />
           <div
             className={`hamburger-white-bar bottom${
-              hamburger === 0? " move" : hamburger > 0 ? " reset" : ""
+              hamburger === 0? " move" : hamburger === 4? ' reverse' : hamburger > 0 ? " reset" :  ''
             }`}
           />
         </div>
         <div
           onClick={() => {
-            setHamburger(0);
-            setTimeout(() => {setHamburger(1)}, 450);
+            if (hamburger === 2) {
+              setHamburger(3)
+              setTimeout(() => {
+                setHamburger(4);
+              },450);
+              return;
+            }
           }}
-          className={`icon-container${hamburger === 2 ? ' show': hamburger === 1 ? ' reset':''}`}
+          className={`icon-container${hamburger === 2 ? ' show': hamburger === 3 ? ' reset':''}`}
         >
           <div
             className={`white-bar left${
-              hamburger === 2 ? " show" : hamburger === 1 ? " reset" : ""
+              hamburger === 2 ? " show" : hamburger === 3 ? " reset" : ""
             }`}
           />
           <div className="inverted-container-right">
             <div
               className={`white-bar right${
-                hamburger === 2 ? " show" : hamburger > 1 ? " reset" : ""
+                hamburger === 2 ? " show" : hamburger === 3 ? " reset" : ""
               }`}
             />
           </div>
