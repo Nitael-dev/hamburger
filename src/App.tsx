dff58564db0918ffad2e3ab3d7ff8037de7240a7
Nitael <nitael@okaydev.com.br>
2022-01-07 15:49:37 -0300

    animation finished(true case)

--- src/App.tsx
+++ src/App.tsx
@@ -4,63 +4,123 @@ import "./hamburger-only.css";
 
 function App() {
   const [hamburger, setHamburger] = React.useState(-1);
-  const a = 1;
+  const a = -1;
   return (
-    <div className="App">
-        <div
-          onClick={() => {
-            if (hamburger === 2) {
-              return;
-            }
-            setHamburger(0);
-            setTimeout(() => {setHamburger(1)}, 290);
-            setTimeout(() => {setHamburger(2)}, 300);
-          }}
-          className={`scale-container${
-            hamburger === 0? " move" : hamburger === 4? ' reverse' : hamburger > 0 ? " reset" :  ''
-          }`}
-        >
+    <div>
+      {a > 0 ? 
+        <div className="App">
           <div
-            className={`hamburger-white-bar top${
+            onClick={() => {
+              if (hamburger === 2) {
+                return;
+              }
+              setHamburger(0);
+              setTimeout(() => {setHamburger(1)}, 290);
+              setTimeout(() => {setHamburger(2)}, 300);
+            }}
+            className={`scale-container${
               hamburger === 0? " move" : hamburger === 4? ' reverse' : hamburger > 0 ? " reset" :  ''
             }`}
-          />
+          >
+            <div
+              className={`hamburger-white-bar top${
+                hamburger === 0? " move" : hamburger === 4? ' reverse' : hamburger > 0 ? " reset" :  ''
+              }`}
+            />
+            <div
+              className={`hamburger-white-bar center${
+                hamburger === 0? " move" : hamburger === 4? ' reverse' : hamburger > 0 ? " reset" :  ''
+              }`}
+            />
+            <div
+              className={`hamburger-white-bar bottom${
+                hamburger === 0? " move" : hamburger === 4? ' reverse' : hamburger > 0 ? " reset" :  ''
+              }`}
+            />
+          </div>
+          <div
+            onClick={() => {
+              if (hamburger === 2) {
+                setHamburger(3)
+                setTimeout(() => {
+                  setHamburger(4);
+                },450);
+                return;
+              }
+            }}
+            className={`icon-container${hamburger === 2 ? ' show': hamburger === 3 ? ' reset':''}`}
+          >
+            <div
+              className={`white-bar left${
+                hamburger === 2 ? " show" : hamburger === 3 ? " reset" : ""
+              }`}
+            />
+            <div className="inverted-container-right">
+              <div
+                className={`white-bar right${
+                  hamburger === 2 ? " show" : hamburger === 3 ? " reset" : ""
+                }`}
+              />
+            </div>
+          </div>
+      </div>
+      : <div className="App">
           <div
             className={`hamburger-white-bar center${
               hamburger === 0? " move" : hamburger === 4? ' reverse' : hamburger > 0 ? " reset" :  ''
             }`}
           />
           <div
-            className={`hamburger-white-bar bottom${
+            onClick={() => {
+              if (hamburger === 2) {
+                return;
+              }
+              setHamburger(0);
+              setTimeout(() => {setHamburger(1)}, 290);
+              setTimeout(() => {setHamburger(2)}, 300);
+            }}
+            className={`scale-container${
               hamburger === 0? " move" : hamburger === 4? ' reverse' : hamburger > 0 ? " reset" :  ''
             }`}
-          />
-        </div>
-        <div
-          onClick={() => {
-            if (hamburger === 2) {
-              setHamburger(3)
-              setTimeout(() => {
-                setHamburger(4);
-              },450);
-              return;
-            }
-          }}
-          className={`icon-container${hamburger === 2 ? ' show': hamburger === 3 ? ' reset':''}`}
-        >
-          <div
-            className={`white-bar left${
-              hamburger === 2 ? " show" : hamburger === 3 ? " reset" : ""
-            }`}
-          />
-          <div className="inverted-container-right">
+          >
             <div
-              className={`white-bar right${
-                hamburger === 2 ? " show" : hamburger === 3 ? " reset" : ""
+              className={`hamburger-white-bar top${
+                hamburger === 0? " move" : hamburger === 4? ' reverse' : hamburger > 0 ? " reset" :  ''
+              }`}
+            />
+            <div
+              className={`hamburger-white-bar bottom${
+                hamburger === 0? " move" : hamburger === 4? ' reverse' : hamburger > 0 ? " reset" :  ''
               }`}
             />
           </div>
-        </div>
+          <div
+            onClick={() => {
+              if (hamburger === 2) {
+                setHamburger(3)
+                setTimeout(() => {
+                  setHamburger(4);
+                },450);
+                return;
+              }
+            }}
+            className={`icon-container${hamburger === 2 ? ' show': hamburger === 3 ? ' reset':''}`}
+          >
+            <div
+              className={`white-bar left${
+                hamburger === 2 ? " show" : hamburger === 3 ? " reset" : ""
+              }`}
+            />
+            <div className="inverted-container-right">
+              <div
+                className={`white-bar right${
+                  hamburger === 2 ? " show" : hamburger === 3 ? " reset" : ""
+                }`}
+              />
+            </div>
+          </div>
+      </div>
+      }
     </div>
   );
 }
